5a2ddd95e2288a02306a5e8824567c79ec5692e8
BuildTools <hareruya_niconico@yahoo.co.jp>
2020-01-17 23:45:39 +0900

    sumitrust2019-A

---
+++ other/sumitrust2019/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  pair<int,int> day1,day2;
+  cin >> day1.first >> day1.second >> day2.first >> day2.second;
+
+  if(day1.first!=day2.first)
+  cout << "1" << endl;
+  else
+  cout << "0" << endl;
+}
