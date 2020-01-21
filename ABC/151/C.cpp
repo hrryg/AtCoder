d827945b58a7acd918e07a6cb8ab51e3b0762ec7
BuildTools <hareruya_niconico@yahoo.co.jp>
2020-01-21 23:11:47 +0900

    ABC151-C

--- ABC/151/C.cpp
+++ ABC/151/C.cpp
@@ -4,27 +4,28 @@ using namespace std;
 int main(){
   int N,M,i,ACcnt=0,WAcnt=0;
   cin >> N >> M;
-  map<int,string> AtCoder;
+  map<int,pair<string,int>> AtCoder;
 
   for(i=0;i<M;i++){
     int p;
     string S;
     cin >> p >> S;
     if(AtCoder.count(p)){
-      if(AtCoder.at(p)=="WA"){
+      if(AtCoder.at(p).first=="WA"){
         if(S=="AC"){
           ACcnt++;
-          AtCoder[p] = "AC";
+          AtCoder[p].first = "AC";
+          WAcnt += AtCoder[p].second;
         }else if(S=="WA")
-        WAcnt++;
+        AtCoder[p].second++;
       }
     }else{
       if(S=="WA"){
-        WAcnt++;
-        AtCoder[p] = "WA";
+        AtCoder[p].first = "WA";
+        AtCoder[p].second = 1;
       }else if(S=="AC"){
         ACcnt++;
-        AtCoder[p] = "AC";
+        AtCoder[p].first = "AC";
       }
     }
   }
