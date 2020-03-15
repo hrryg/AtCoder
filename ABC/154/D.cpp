8cbbafee24c0891a0402b5caf6f84c0d91638571
hrryg <55384190+hrryg@users.noreply.github.com>
2020-03-15 20:40:51 +0900

    [update] ABC154-D

--- ABC/154/D.cpp
+++ ABC/154/D.cpp
@@ -1,27 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main(){
-  double N,K,i,j,num,mnum;
+  double N,K,i,j=0,num,mnum=0;
   cin >> N >> K;
-  vector<pair<double,double>> p(N);
+  vector<double> p(N);
 
-  for(i=0;i<N;i++)
-  cin >> p.at(i).first;
   for(i=0;i<N;i++){
-    num = (1 + p.at(i).first) * p.at(i).first / 2;
-    num = num/p.at(i).first;
-    p.at(i).second = num;
+    int n;
+    cin >> n;
+    num = (1 + n) * n / 2;
+    num = num/n;
+    p.at(i)= num;
   }
   for(i=0;i<K;i++)
-  mnum += p.at(i).second;
-  for(i=1;i<N-K+1;i++){
-    num = 0;
-    for(j=i;j<i+K;j++){
-      num += p.at(j).second;
-    }
-    mnum = max(mnum,num);
-  }
-  cout << mnum;
+    mnum += p.at(i);
+  num = mnum;
 
+  for(i=K;i<N;i++){
+    num = num+p.at(i)-p.at(j);
+    mnum = max(mnum,num);
+    ++j;
+  }
+  cout << setprecision(10) << mnum;
 }
